597a9719ba4d796f7ec8d6ca86e4d29f8f820e09
Saka Ricky <sakaricky91@gmail.com>
2020-12-11 12:06:40 +0100

    added console.log to app.js to see why server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
-const routes = require('./routes')
+const routes = require('./routes');
 
-const server = http.createServer(routes)
+const server = http.createServer(routes);
+console.log("Server started");
 
 server.listen(3000); 
